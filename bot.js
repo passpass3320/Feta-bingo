e8be0b661e996bd64f1de49532fd27008312faf4
passpass3320 <PassPass3320@gmail.com>
2026-08-12 03:07:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,23 @@
+const { Telegraf, Markup } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Telegram /start command
+bot.start((ctx) => {
+  const appUrl = process.env.APP_URL;
+
+  ctx.replyWithMarkdown(
+    `*እንኳን ወደ ዘመን ቢንጎ በደህና መጡ!* 🎲\n\nለመጫወት ከታች ያለውን *Play Bingo* ቁልፍ ይጫኑ።`,
+    Markup.inlineKeyboard([
+      [Markup.button.webApp('🎮 Play Bingo', appUrl)]
+    ])
+  );
+});
+
+bot.launch().then(() => console.log('🤖 Telegram Bot Running!'));
+
+// Graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
